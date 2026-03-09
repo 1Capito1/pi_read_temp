3f8f3b23febce3f81074d431d6a1f7444504f3a5
Capito <seth.l.tootell@gmail.com>
2026-03-10 10:49:49 +1100

    keyboard interrupt

--- read_temp.py
+++ read_temp.py
@@ -4,10 +4,16 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-while (True):
+def get_temp():
     t = sense.temp
     temp = 0.0 if not isinstance(t, (int, float)) else float(t)
-    text = str(temp)
+    text = f"{temp:.1f}"
     sense.show_message(text)
     sleep(5)
 
+while (True):
+    try:
+        get_temp()
+    except KeyboardInterrupt:
+        print("exiting gracefully")
+        sense.clear
